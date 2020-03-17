6bec5a878fe97c0274264295979ed2455204fb0a
Amarthya <amarthyaseshu2@gmail.com>
2020-03-17 12:54:57 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // Important Note - No Built-in functions to be used
 // Progression 1: Names and Input
-
 // 1.1 Create a variable `ProGrad-1` with the driver's name.
 // 1.2 Print `"The driver's name is XXXX"`.
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
+
+var ProGrad_1;
+ProGrad_1 = "driver";
+document.write("The driver's name is " + ProGrad_1);
+ProGrad_2 = "navigator";
+document.write("<br>");
+document.write("The navigator's name is " + ProGrad_2);
 
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
@@ -12,14 +18,81 @@
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
 
+let d, n;
+d = ProGrad_1.length;
+n = ProGrad_2.length;
+if (d > n) {
+    document.write("<br>");
+    document.write("The driver has the longest name, it has " + d + "characters");
+} else if (d < n) {
+    document.write("<br>");
+    document.write("The navigator has the longest name, it has " + n + " characters");
+
+} else {
+    document.write("<br>");
+    document.write("Wow, you both have equally long names, " + n + "characters!");
+}
+
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the name, and also print the vowel letters along with the vowel index. or
 // - print no vowels
 // - for example. In String ProGrad - o and a are vowels. Print ProGrad o a 2 5. 
 
+var a = "ProGrad";
+var i;
+if (a.match(/[aeiouAEIOU]/)) {
+    document.write("<br>");
+    document.write(a + "&nbsp;");
+    b = a.length;
+    for (i = 0; i < b; i++) {
+        if (a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u' || a.charAt(i) == 'A' || a.charAt(i) == 'E' || a.charAt(i) == 'I' || a.charAt(i) == 'O' || a.charAt(i) == 'U') {
+            document.write(a.charAt(i) + "&nbsp;");
+        }
+    }
+} else {
+    document.write("<br>");
+    document.write("no vowels");
+}
+
+
+
+
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
+var count = 0;
+var countlow = 0;
+
+if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z')) {
+    document.write("<br>");
+    document.write("string contains uppercase and lowercase characters");
+} else {
+    document.write("<br>");
+    document.write("string does not contains uppercase and lowercase characters");
+}
+
+for (i = 0; i < b; i++) {
+    if (a.charAt(i) >= 'A' && a.charAt(i) <= 'Z')
+        count++;
+}
+document.write("<br>");
+document.write("the number of uppercase characters are " + count);
+for (i = 0; i < b; i++) {
+    if (a.charAt(i) >= 'a' && a.charAt(i) <= 'z')
+        countlow++;
+}
+document.write("<br>");
+document.write("the number of lowercase characters are " + countlow);
+
+
+
+
+
+
+
+
+
+
 
 
 // Progression 3: Control Statements - 2
