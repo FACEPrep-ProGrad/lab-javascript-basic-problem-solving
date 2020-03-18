1bdebb6f2056c4a656feb249ba72558da6655247
Deepankar Tiwari <deepankar_tiwari@outlook.com>
2020-03-18 12:02:36 +0530

    Progression 3.2

--- app.js
+++ app.js
@@ -103,9 +103,6 @@ console.log(ProGrad_2 + " " + ProGrad_1);
 // Make your program count the number of words in the string.
 // Make your program count the number of times the Latin word et appears.
 
-
-
-
 // Bonus 2:
 // Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
